d5003f74537db2d3fe0277a7f42c6cdefe1e0db0
Edward Betts <edward@4angle.com>
2021-02-07 20:16:13 +0000

    Correct spelling mistakes

--- src/faux86/Audio.h
+++ src/faux86/Audio.h
@@ -70,7 +70,7 @@ struct wav_hdr_s {
 	uint8_t	fmt[4];	 /* FMT header	   */
 	uint32_t Subchunk1Size;  /* Size of the fmt chunk				*/
 	uint16_t AudioFormat;	/* Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM */
-	uint16_t NumOfChan;	  /* Number of channels 1=Mono 2=Sterio		   */
+	uint16_t NumOfChan;	  /* Number of channels 1=Mono 2=Stereo		   */
 	uint32_t SamplesPerSec;  /* Sampling Frequency in Hz				 */
 	uint32_t bytesPerSec;	/* bytes per second */
 	uint16_t blockAlign;	 /* 2=16-bit mono, 4=16-bit stereo */
